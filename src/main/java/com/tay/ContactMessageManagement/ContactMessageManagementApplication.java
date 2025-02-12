7e7a8823d9f50a4fd4d7f1726f7d15d46bad1dd3
Yasar TAY <yasartay@gmail.com>
2025-02-12 23:11:42 +0100

    First Commit: Dependencies, application.properties and Database is ready. App is working.

---
+++ src/main/java/com/tay/ContactMessageManagement/ContactMessageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tay.ContactMessageManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMessageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMessageManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tay/ContactMessageManagement/ContactMessageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tay.ContactMessageManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMessageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
