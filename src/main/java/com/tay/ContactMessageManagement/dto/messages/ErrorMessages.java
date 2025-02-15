1d3bf09117feb9e74859b467a051f07e7a8cda85
Yasar TAY <yasartay@gmail.com>
2025-02-15 23:31:04 +0100

    minor tweaks reverted

--- src/main/java/com/tay/ContactMessageManagement/dto/messages/ErrorMessages.java
+++ src/main/java/com/tay/ContactMessageManagement/dto/messages/ErrorMessages.java
@@ -10,5 +10,7 @@ public class ErrorMessages {
     public static final String NOT_FOUND_BY_DATE = "No message found between dates: %s - %s";
     public static final String NOT_FOUND_BY_TIME = "No message found between times: %s - %s";
     public static final String NOT_FOUND_BY_ID = "No message found with ID: %s";
+    public static final String BAD_REQUEST_DATE = "Invalid date! Please enter a valid date in yyyy-MM-dd.";
+    public static final String BAD_REQUEST_TIME = "Invalid time! Please enter a valid time in HH:mm.";
 
 }
